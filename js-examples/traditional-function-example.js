71a11467ab37604032170e5fc05af3e3a4ea7422
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-09 08:07:36 -0400

    Create traditional-function-example.js

---
+++ js-examples/traditional-function-example.js
@@ -0,0 +1,36 @@
+// Abraham Lincoln
+// 10 APR 20XX
+// Traditional Function Examples
+
+// Function to calculate sum of two numbers
+function sumTwoNumbers(a, b) {
+    // Return the sum of the two input parameters
+    return a + b;
+}
+
+const num1 = 5;
+const num2 = 3;
+let finalAnswer = sumTwoNumbers(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is ${finalAnswer}.`);
+
+// Function to welcome user by first name
+function welcomeUser(firstName) {
+    // Return a welcome message with the user's first name
+    return `Welcome, ${firstName}!`;
+}
+
+const userName = "Alyssa";
+const greeting = welcomeUser(userName);
+console.log(greeting);
+
+
+// Function to calculate and return area of a rectangle
+function calculateRectangleArea(length, width) {
+    // Return the product of the length and width to get the area
+    return length * width;
+}
+
+let rectLength = 4;
+let rectWidth = 6;
+let rectArea = calculateRectangleArea(rectLength, rectWidth);
+console.log(`Area of your rectangle (sq. ft.) is: ${rectArea}`);
