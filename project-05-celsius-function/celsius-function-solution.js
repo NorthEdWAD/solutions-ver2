067f34a163ee15e5cdb262882490f8e2685dc381
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-09 11:47:37 -0400

    Update celsius-function-solution.js

--- project-05-celsius-function/celsius-function-solution.js
+++ project-05-celsius-function/celsius-function-solution.js
@@ -1,1 +1,20 @@
+// Abraham Lincoln
+// 10 JAN 20XX
+// Celsius Function
+
+// Write function to convert F to C 
+function convertTemperature(temperature) {
+  return (temperature - 32) / (9/5);
+};
+
+// Get Fahrenheit temperature from user
+
+fahrenheitTemp = Number(input('Please enter your Fahrenheit temperature (Example: 32):\n'));
+
+// Call the convertTemperature( ) function to do the math
+celsiusTemp = convertTemperature(fahrenheitTemp);
+
+// Display output in alert box
+outputString = `${fahrenheitTemp} degrees Fahrenheit is equivalent to ${celsiusTemp} degrees Celsius.`;
+alert(outputString);
 
