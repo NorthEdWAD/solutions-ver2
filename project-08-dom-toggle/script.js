6edcf227462a643865f64e9cd3bd657aa0bc9454
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-10 12:36:17 -0400

    Create script.js

---
+++ project-08-dom-toggle/script.js
@@ -0,0 +1,11 @@
+// First and last name
+// Current date
+// JS Traffic Light Project (DOM)
+
+// Objective
+// Create a traffic light simulator using JS
+
+// TODO 1: Declare using `let` a variable named lightColor
+
+// TODO 2: Use an if-else statement to help JavaScript update the color of a DIV on your screen
+// If the lightColor variable is set to `green`. change the 
