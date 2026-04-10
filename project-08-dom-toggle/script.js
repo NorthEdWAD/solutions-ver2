0b5d0b493e34ec889dfe0ae310f62e5e96eec8d4
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-10 12:39:33 -0400

    Update script.js

--- project-08-dom-toggle/script.js
+++ project-08-dom-toggle/script.js
@@ -5,8 +5,8 @@
 // Objective
 // Create a traffic light simulator using JS
 
-// TODO 1: Declare using `let` a variable named lightColor
+// TODO 1: Declare using let a variable named lightColor
 
 // TODO 2: Use an if-else statement to help JavaScript update the color of a DIV on your screen
-// If the lightColor variable is set to `green`. change the corresponding `<div>` on the screen to green
-// Otherwise, if `lightColor` is set to `red`, change the corresponding `<div>` on the screen to red
+// If the lightColor variable is set to green,  change the corresponding <div> on the screen to green
+// Otherwise, if lightColor is set to red, change the corresponding <div> on the screen to red
