47cd49b6b04e5563773aa91209370b30a60379b3
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-10 12:38:24 -0400

    Update script.js

--- project-08-dom-toggle/script.js
+++ project-08-dom-toggle/script.js
@@ -8,4 +8,5 @@
 // TODO 1: Declare using `let` a variable named lightColor
 
 // TODO 2: Use an if-else statement to help JavaScript update the color of a DIV on your screen
-// If the lightColor variable is set to `green`. change the 
+// If the lightColor variable is set to `green`. change the corresponding `<div>` on the screen to green
+// Otherwise, if `lightColor` is set to `red`, change the corresponding `<div>` on the screen to red
