982d9a16b5a5825034eb37dd7614a5bd92b812bc
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-15 09:56:02 -0400

    Edited .js for Part 1 of Pizza Order project

--- project-09-js-template/pizza-order-part1.js
+++ project-09-js-template/pizza-order-part1.js
@@ -1,14 +1,15 @@
 // First and last name
 // Current date
-// JS Template File
+// JS Pizza Order (Part 1)
 
 
 // INPUT
-const username = prompt("Please enter your username:\n");
-const timeOfDay = prompt("What time of day is it now? (Example: morning)\n");
+const size = prompt("Enter size of pizza (small/medium/large):\n");
+const topping1 = prompt("Enter first topping (Example: mushrooms)\n");
+const topping2 = prompt("Enter second topping (Example: pepperoni)\n");
 
 // PROCESS
-const msg = `Hello, ${username}! Have a great ${timeOfDay}!`;
+const msg = `Your ${size} pizza with ${topping1} and ${topping1} will be ready soon!`;
 
 // OUTPUT
 alert(msg);
