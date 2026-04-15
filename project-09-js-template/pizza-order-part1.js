211cff03e263184563b4b9f4b76511f4f55f4a69
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-15 10:01:35 -0400

    Added event listener to pizza order JS

--- project-09-js-template/pizza-order-part1.js
+++ project-09-js-template/pizza-order-part1.js
@@ -2,14 +2,22 @@
 // Current date
 // JS Pizza Order (Part 1)
 
+// Add event listener to run placeOrder function
+document.getElementById("btn").addEventListener("click", placeOrder);
 
-// INPUT
-const size = prompt("Enter size of pizza (small/medium/large):\n");
-const topping1 = prompt("Enter first topping (Example: mushrooms)\n");
-const topping2 = prompt("Enter second topping (Example: pepperoni)\n");
+// Create function to place pizza order
+function placeOrder(){
+    // INPUT
+    const size = prompt("Enter size of pizza (small/medium/large):\n");
+    const topping1 = prompt("Enter first topping (Example: mushrooms)\n");
+    const topping2 = prompt("Enter second topping (Example: pepperoni)\n");
 
-// PROCESS
-const msg = `Your ${size} pizza with ${topping1} and ${topping1} will be ready soon!`;
+    // PROCESS
+    const msg = `Your ${size} pizza with ${topping1} and ${topping2} will be ready soon!`;
 
-// OUTPUT
-alert(msg);
+    // OUTPUT
+    alert(msg);
+}
+
+
+
