f1fd4031ef7a1b8b831bb07b780b4fa4130ef90f
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-15 09:49:26 -0400

    Added starter files for Pizza Order (Part 1)

---
+++ project-09-js-template/pizza-order-part1.js
@@ -0,0 +1,14 @@
+// First and last name
+// Current date
+// JS Template File
+
+
+// INPUT
+const username = prompt("Please enter your username:\n");
+const timeOfDay = prompt("What time of day is it now? (Example: morning)\n");
+
+// PROCESS
+const msg = `Hello, ${username}! Have a great ${timeOfDay}!`;
+
+// OUTPUT
+alert(msg);
