4fd455ba33984f2656f4834b89db60147531894f
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-15 11:05:28 -0400

    Edited JS for addition calculator

--- project-10-simple-add-calculator/addition-calculator.js
+++ project-10-simple-add-calculator/addition-calculator.js
@@ -5,11 +5,16 @@
 // Add event listener to button element
 document.getElementById("btn").addEventListener("click", btnClicked);
 
-// Create function to place pizza order
+// Create function to make Calculate button work
 function btnClicked(){
     // INPUT
-    const num1 = document.getElementById("num1-in").value;
-    const num2 = document.getElementById("num2-in").value;
+    let num1 = document.getElementById("num1-in").value;
+    let num2 = document.getElementById("num2-in").value;
+
+    // Convert text input into numbers so JS can do math for us
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
 
     // PROCESS
     const total = num1 + num2;
