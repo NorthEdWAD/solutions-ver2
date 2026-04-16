6a9554512e029dfda76138edcc37581bca5c26c7
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-16 15:42:24 -0400

    Update functions-debugging.js

--- project-05-celsius-function/functions-debugging.js
+++ project-05-celsius-function/functions-debugging.js
@@ -3,10 +3,48 @@
 // Function Debug Activity
 
 // Example 1
-function 
+function sayHello(userName) {
+  let message = "Hello, " + username + "!";
+  const message = `Hello, ${username}!`;
+  alert(messge);
+}
 
 
 // Example 2
-
+// 2 errors
+function changeColor(newColor)
+  document.body.style.background-color = newColor;
+}
 
 // Example 3
+function calculateScore(level) {
+  const score = level * "10";
+  return score;
+
+
+// Example 4
+function swapImage() {
+  var img = document.getElementById("myPhoto");
+  img.src = "sunset.jpg"
+  console.log("Image swapped!)
+}
+
+---
+
+// Example 5
+let playerScore;
+playerScore = 0;
+
+
+// Example 6
+let playerScore;
+playerScore = 20;
+playerScore = 30;
+
+// Example 7
+const gameLevel = 5;
+gameLevel = 7;
+
+// Example 8
+const maxLives;
+maxLives = 3;
